fa76de167b5ff9e6f429e6bbe3e3dd7973517d0d
Ben Tyler <tylerben14@gmail.com>
2021-05-18 11:48:53 -0700

    fix: loading icon max width

--- frontend/src/components/Loading/Loading.js
+++ frontend/src/components/Loading/Loading.js
@@ -1,23 +1,26 @@
-import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import loading from "../../images/loading.svg";
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import loading from '../../images/loading.svg';
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(theme => ({
   root: {
-    position: "absolute",
-    display: "flex",
-    justifyContent: "center",
-    height: "100vh",
-    width: "100vw",
+    position: 'absolute',
+    display: 'flex',
+    justifyContent: 'center',
+    height: '100vh',
+    width: '100vw',
     top: 0,
     bottom: 0,
     left: 0,
     right: 0,
-    backgroundColor: "white",
+    backgroundColor: 'white',
+    '& img': {
+      maxWidth: 150,
+    },
   },
 }));
 
-const Loading = (props) => {
+const Loading = props => {
   const classes = useStyles();
 
   return (
